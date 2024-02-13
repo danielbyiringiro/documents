be4c9d6f49de3cd0dd923d6d9437b6027ce0c716
danielbyiringiro <daniel.byiringiro@ashesi.edu.gh>
2024-02-13 12:47:01 +0000

    init

---
+++ c/helloworld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>  //include information about the standard library
+
+int main() // define a function named main tat receives no arguement values
+{
+   // statements of main are enclosed in braces
+
+   printf("Hello, Wordl!\n");
+   
+//    main calls a library function printf to print the sequences of    //    characters. \n represents the newline character.
+//
+}
